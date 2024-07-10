e564f42ec86df2eb26d4af5b93c00ae924e5327d
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-07-11 00:25:15 +0500

    tugadi

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const bodi=document.getElementsByTagName('body')
+const text = document.getElementsByTagName('p');
+let conti= document.querySelectorAll('continer')
+let header= document.getElementsByClassName('header_continer')
+let ullist= document.getElementsByClassName('olList')
+let button= document.querySelector('#batton')
+let fcontiner= document.getElementsByClassName('fast_continer')
+console.log(button)
