a580f83815a4f96ec2310dcef1192ef11f025e2d
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-07-11 08:43:03 +0500

    tugadi

--- js/index.js
+++ js/index.js
@@ -1,8 +1,17 @@
-const bodi=document.getElementsByTagName('body')
+const bodi=document.querySelector('body')
 const text = document.getElementsByTagName('p');
 let conti= document.querySelectorAll('continer')
 let header= document.getElementsByClassName('header_continer')
 let ullist= document.getElementsByClassName('olList')
-let button= document.querySelector('#batton')
+// let button= document.querySelector('#batton')
 let fcontiner= document.getElementsByClassName('fast_continer')
-console.log(button)
+// console.log(button)
+
+const buttonblac = document.querySelector('#batton-black');
+
+buttonblac.addEventListener('click', function(event){
+    event.preventDefault();
+    bodi.style.backgroundColor = 'black';
+})
+
+// console.log(buttonblac)
