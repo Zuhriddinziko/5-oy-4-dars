5900681c3140848904f2edffcfb2c4f492b88c1e
Zuhriddinziko <169124288+Zuhriddinziko@users.noreply.github.com>
2024-07-11 09:12:11 +0500

    tt

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
-const bodi=document.querySelector('body')
-const text = document.getElementsByTagName('p');
-let conti= document.querySelectorAll('continer')
-let header= document.getElementsByClassName('header_continer')
-let ullist= document.getElementsByClassName('olList')
+const bodi=document.querySelector('body');
+const text = document.querySelector('p');
+let conti= document.querySelectorAll('continer');
+const tex= document.querySelectorAll('#tex');
+let header= document.getElementsByClassName('header_continer');
+let ullist= document.getElementsByClassName('olList');
 // let button= document.querySelector('#batton')
-let fcontiner= document.getElementsByClassName('fast_continer')
-// console.log(button)
+let fcontiner= document.getElementsByClassName('fast_continer');
+console.log(tex)
 
 const buttonblac = document.querySelector('#batton-black');
+const buttonwhil = document.querySelector('#batton-while');
 
 buttonblac.addEventListener('click', function(event){
     event.preventDefault();
-    bodi.style.backgroundColor = 'black';
+    bodi.style.backgroundColor = 'red';
+    tex.style.color = 'while';
 })
-
+buttonwhil.addEventListener('click', function(event){
+    event.preventDefault();
+    bodi.style.backgroundColor = '#EBEBEB';
+    tex.style.color = 'while';
+})
 // console.log(buttonblac)
